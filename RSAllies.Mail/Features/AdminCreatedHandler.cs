8852d43fd10d539f14367aa43b274f2d0f577382
hei karata <herishine00@gmail.com>
2024-06-14 18:56:04 +0300

    Improve email error handling in AdminCreatedHandler
    - Added a try-catch block in `AdminCreatedHandler.cs` for better error handling during the email sending process.
    - Encapsulated email sending logic within a try block to catch potential exceptions.
    - Modified `MimeMessage` setup for improved clarity and readability.
    - Implemented simple error logging by printing exceptions to the console.

--- RSAllies.Mail/Features/AdminCreatedHandler.cs
+++ RSAllies.Mail/Features/AdminCreatedHandler.cs
@@ -11,18 +11,25 @@ namespace RSAllies.Mail.Features
         {
             if (!string.IsNullOrEmpty(notification.Email))
             {
-                var message = new MimeMessage();
-                message.From.Add(new MailboxAddress("DoNotReply", "donotreply@roadsafetyallies.me"));
-                message.To.Add(new MailboxAddress(notification.Username, notification.Email));
-                message.Subject = "Welcome to DCTTS Site Administration";
-                message.Body = new TextPart(MimeKit.Text.TextFormat.Plain)
+                try
                 {
-                    Text = $"Hello, Join us at Site Administration " +
-                           $"your username is {notification.Username} and " +
-                           $"your password is {notification.Password}. " +
-                           $"Do not share your credentials "
-                };
-                await smtpClient.SendAsync(message, cancellationToken);
+                    var message = new MimeMessage();
+                    message.From.Add(new MailboxAddress("DoNotReply", "donotreply@roadsafetyallies.me"));
+                    message.To.Add(new MailboxAddress(notification.Username, notification.Email));
+                    message.Subject = "Welcome to DCTTS Site Administration";
+                    message.Body = new TextPart("plain")
+                    {
+                        Text = $"Hello, Join us at Site Administration " +
+                               $"your username is {notification.Username} and " +
+                               $"your password is {notification.Password}. " +
+                               $"Do not share your credentials "
+                    };
+                    await smtpClient.SendAsync(message, cancellationToken);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
             }
         }
     }
