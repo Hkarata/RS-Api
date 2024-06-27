129fcf6d8e63f36bb46044f6b09a5ebf30cb9456
hei karata <herishine00@gmail.com>
2024-06-28 02:04:18 +0300

    Enhance security and update seed data GUIDs
    - Removed the maximum length constraint on the `Password` field in UsersDbContextModelSnapshot.cs, changing its type to `nvarchar(max)` to support more secure hashing algorithms.
    - Updated predefined GUIDs in seed data for entities like `EducationLevels`, `Genders`, `LicenseClasses`, and `Roles` to facilitate database migration, reseeding, or correction of seed data errors.
    - Added a new migration, `PasswordMigration`, to modify the `Password` column in the `Administrators` table from `nvarchar(20)` to `nvarchar(max)`, aligning with secure password storage practices.
    - Included EF Core metadata in `PasswordMigration.Designer.cs` to accurately track and apply schema changes.

---
+++ RSAllies.Users/Migrations/20240627230155_PasswordMigration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RSAllies.Users.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasswordMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                schema: "Users",
+                table: "Administrators",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                schema: "Users",
+                table: "Administrators",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
