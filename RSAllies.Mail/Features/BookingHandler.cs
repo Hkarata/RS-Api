8b091d9690be5c4f3aacf81f09a9c5d95818e45f
hei karata <herishine00@gmail.com>
2024-06-30 15:48:33 +0300

    Update DB connection and format SQL queries
    - Updated the database connection string in `appsettings.json` to point to a remote server (4.221.76.213) instead of localhost, indicating a shift to a remote database setup.
    
    - Improved SQL query readability in `BookingHandler.cs`, `PaymentMadeHandler.cs`, and `PaymentNumberCreatedHandler.cs` within the `RSAllies.Mail.Features` namespace by adding extra spaces around the `JOIN` clause. This change enhances code readability without affecting functionality.

--- RSAllies.Mail/Features/BookingHandler.cs
+++ RSAllies.Mail/Features/BookingHandler.cs
@@ -44,8 +44,8 @@ namespace RSAllies.Mail.Features
         public async Task<User> GetUserDetailsAsync(Guid userId)
         {
             var query = $"SELECT CONCAT(u.FirstName, ' ', u.MiddleName, ' ', u.LastName) AS Name, a.Email AS Email " +
-                        $"FROM Users.Users u" +
-                        $"Join Users.Accounts a ON u.Id = a.Id" +
+                        $"FROM Users.Users u "+
+                        $"Join Users.Accounts a ON u.Id = a.Id "+
                         $"WHERE u.Id = @UserId";
 
             var userIdParameter = new SqlParameter("@UserId", userId);
