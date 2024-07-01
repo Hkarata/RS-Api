8816f9a5cb3589d1fa22613dd6ea2b64a8d192c1
hei karata <herishine00@gmail.com>
2024-07-01 10:44:32 +0300

    Add CloseCase feature with new dependencies
    This commit introduces a new feature, `CloseCase`, within the `RSAllies.Users.Features.Support` namespace, designed to handle the closing of support cases. It adds several dependencies, including `Carter`, `MediatR`, and various `Microsoft.AspNetCore.*` namespaces, to support routing, database interactions, and request handling. The implementation includes a `Command` class for the request model, a `Handler` class for processing the command using Entity Framework Core to interact with the `SupportCases` table, and basic error handling. The outcome of the operation is encapsulated in a `Result` object, adhering to the CQRS pattern. Additionally, a new endpoint is defined in the `CloseCaseEndPoint` class to listen for DELETE requests, demonstrating integration with the MediatR library and asynchronous programming patterns for improved scalability and performance.

---
+++ RSAllies.Users/Features/Support/CloseCase.cs
@@ -0,0 +1,54 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using RSAllies.Shared.HelperTypes;
+using RSAllies.Users.Data;
+using RSAllies.Users.Features.Support;
+
+namespace RSAllies.Users.Features.Support
+{
+    internal class CloseCase
+    {
+        internal class Command : IRequest<Result>
+        {
+            public Guid CaseId { get; set; }
+        }
+
+        internal sealed class Handler(UsersDbContext context) : IRequestHandler<Command, Result>
+        {
+            public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var supportCase = await context.SupportCases
+                        .Where(c => c.Id == request.CaseId && !c.IsClosed)
+                        .SingleOrDefaultAsync(cancellationToken);
+
+                if (supportCase is null)
+                {
+                    return Result.Failure(Error.NullValue);
+                }
+
+                supportCase.IsClosed = true;
+
+                await context.SaveChangesAsync(cancellationToken);
+
+                return Result.Success();
+            }
+        }
+    }
+}
+
+public class CloseCaseEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/api/support-case/{caseId:guid}", async (Guid caseId, ISender sender) =>
+        {
+            var request = new CloseCase.Command { CaseId = caseId };
+            var result = await sender.Send(request);
+            return result.IsSuccess ? Results.Ok(result) : Results.Ok(result.Error);
+        });
+    }
+}
